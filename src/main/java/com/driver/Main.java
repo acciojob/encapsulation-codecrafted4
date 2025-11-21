cc82caac6236a550dcb13da7b52b8ecfcf352300
Utkarsh Mishra <utkarshmishra171198@gmail.com>
2025-11-21 17:16:30 +0530

    code updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly stud1 = new RWOnly(1,"utkarsh","MP",25);
+        RWOnly stud2 = new RWOnly(2,"satyam","Up",26);
+        RWOnly stud3 = new RWOnly(3,"malay","Haryana",25);
+        RWOnly stud4 = new RWOnly(4,"rahul","Rjasthan",22);
+        RWOnly stud5 = new RWOnly(5,"rekha","Karntka",24);
+
+        stud1.getDetails();
+        stud2.getDetails();
+        stud3.getDetails();
+        stud4.getDetails();
+        stud5.getDetails();
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int id ;
+    private String name;
+    private String address;
+    private int age;
+
+    public RWOnly(int id, String name,String address, int age){
+        this.id = id;
+        this.name = name;
+        this.address= address;
+        this.age = age;
+    }
+    public int getId(){ return this.id;}
+    public String getName(){return this.name;}
+    public String getAddress(){return this.address;}
+    public int getAge(){return this.age;}
+    public void getDetails(){
+        System.out.println("Student-"+getId()+" || "+getName()+" || "+getAddress()+" || "+getAge());
+    }
 }
